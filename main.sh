24e7bc13c7986211e8226e67f7959718e510eff2
qulx <qulx@1jz.cc>
2025-04-29 22:07:21 +0300

    FEATURE: Support custom paths, FIX: Renaming @sessiony-key to @sessiony-bind

--- main.sh
+++ main.sh
@@ -1,11 +1,28 @@
-Sessions=$(tmux ls -F "#S")
+sessions=$(tmux ls -F "#S")
+
+# Collecting params, parsing, appending to paths
+paths=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --paths)
+        IFS=',' read -r -a paths <<<"$2"
+        shift 2
+        ;;
+    *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+done
+
+# Getting user input with fzf-tmux
 CHOOSEN=$(
     tmux ls -F "#S" | fzf-tmux -p 60%,60% --border --margin=0 \
         --header '  A-1 Current Sessions  A-2 Directories  A-d kill ' \
         --no-sort --ansi --border-label ' Sessiony ' --prompt '  ' --border=rounded \
         --bind "tab:down,btab:up" \
         --bind "alt-1:change-prompt(  )+reload(tmux ls -F \"#S\")" \
-        --bind "alt-2:change-prompt(  )+reload(find -L \"$HOME/Dev\" \"$HOME/.config\" -depth -maxdepth 1 -type d)" \
+        --bind "alt-2:change-prompt(  )+reload(find -L ${paths[*]} -depth -maxdepth 1 -type d)" \
         --bind "alt-d:execute(tmux kill-session -t {})+change-prompt(  )+reload(tmux ls -F \"#S\")" \
         --bind "alt-k:up,alt-j:down" \
         --bind alt-\':abort
@@ -16,7 +33,7 @@ if [ "$CHOOSEN" == "" ]; then
 fi
 
 ESCAPED_CHOOSEN=$(echo "$CHOOSEN" | tr "." "_")
-CHOOSEN_EXISTS=$(echo "$Sessions" | grep "$CHOOSEN")
+CHOOSEN_EXISTS=$(echo "$sessions" | grep "$CHOOSEN")
 if [ -z "$CHOOSEN_EXISTS" ]; then
     tmux new -d -s "$ESCAPED_CHOOSEN" -c "$CHOOSEN"
     tmux switchc -t "$ESCAPED_CHOOSEN"
--- sessiony.tmux
+++ sessiony.tmux
@@ -2,8 +2,8 @@
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ -z "$(tmux display-message -p "#{@sessiony-key}")" ]; then
-    tmux bind-key k run-shell "$CURRENT_DIR/main.sh"
+if [ -z "$(tmux display-message -p "#{@sessiony-bind}")" ]; then
+    tmux bind-key k run-shell "$CURRENT_DIR/main.sh --paths #{@sessiony-custom-paths}"
 else
-    tmux bind-key "$(tmux display-message -p "#{@sessiony-key}")" run-shell "$CURRENT_DIR/main.sh"
+    tmux bind-key "$(tmux display-message -p "#{@sessiony-key}")" run-shell "$CURRENT_DIR/main.sh --paths #{@sessiony-custom-paths}"
 fi
